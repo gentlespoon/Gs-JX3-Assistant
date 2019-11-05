670f72a734db9bcd517464d5ce86fba3c4797c68
Angda Song <angda_song@symantec.com>
2019-11-04 17:33:26 -0800

    MouseEvents

--- GsJX3NonInjectAssistant/Classes/MouseEvents.cs
+++ GsJX3NonInjectAssistant/Classes/MouseEvents.cs
@@ -63,6 +63,8 @@ namespace GsJX3NonInjectAssistant
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
         public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        public static extern void send_input();
+
         //Mouse actions
         private const int MOUSEEVENTF_LEFTDOWN = 0x02;
         private const int MOUSEEVENTF_LEFTUP = 0x04;
